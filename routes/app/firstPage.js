9aa5f6a07a07d806186b545611a2231c8308c8e3
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-01-19 11:22:27 +0000

    change in statcounter widget

--- routes/app/firstPage.js
+++ routes/app/firstPage.js
@@ -3,9 +3,6 @@ var router = express.Router();
 
 var config = require('../../config.js');
 
-router.get('*',function(req,res){ 
-    res.redirect(config.currentRoot.substring(0, config.currentRoot.length - 1) + req.url)
-});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/index');
