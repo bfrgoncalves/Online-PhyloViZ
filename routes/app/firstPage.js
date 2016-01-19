e269b16e33bc5e743875c18aa44b91c06441e7a5
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-01-19 11:48:46 +0000

    change in statcounter widget

--- routes/app/firstPage.js
+++ routes/app/firstPage.js
@@ -3,19 +3,6 @@ var router = express.Router();
 
 var config = require('../../config.js');
 
-var https_redirect = function(req, res, next) {
-  if (req.connection.encrypted) {
-    return next();
-  } else {
-  	return res.redirect(config.currentRoot.substring(0, config.currentRoot.length - 1) + req.url);
-  }
-};
-
-//redirect if not secure
-router.get('*', function(req, res, next) {
-  https_redirect(req, res, next);
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/index');
