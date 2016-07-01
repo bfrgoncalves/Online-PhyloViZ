721f12d3a0907adc41db4173a9ff3fa3cbecd883
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-07-01 16:33:54 +0100

    added option to create sharable links without making data sets public

--- remote_upload/remoteUpload.py
+++ remote_upload/remoteUpload.py
@@ -16,6 +16,7 @@ def main():
 	parser.add_argument('-u', nargs='?', type=str, help="username", required=True)
 	parser.add_argument('-p', nargs='?', type=str, help="password", required=True)
 	parser.add_argument('-e', nargs='?', type=bool, help="Make public", required=False, default=False)
+	parser.add_argument('-l', nargs='?', type=bool, help="Public Link", required=False, default=False)
 	parser.add_argument('-sdt', nargs='?', type=str, help='Sequence data type (newick, profile, fasta)', required=True)
 	parser.add_argument('-sd', nargs='?', type=str, help='Sequence data', required=True)
 	parser.add_argument('-m', nargs='?', type=str, help="Metadata", required=False)
@@ -29,6 +30,11 @@ def main():
 	checkDatasets(args, currentRoot)
 	dataset = remoteUpload(args, currentRoot)
 	rungoeBURST(args, dataset['datasetID'], currentRoot)
+
+	if not args.e and args.l:
+		sharableLink = generatePublicLink(args, dataset['datasetID'], currentRoot)
+		print 'Sharable link: ' + sharableLink['url']
+		sys.exit()
 
 	if args.e:
 		print "\nAccess the tree at: " + currentRoot + '/main/dataset/' + dataset['datasetID']
@@ -119,6 +125,25 @@ def rungoeBURST(args, datasetID, currentRoot): #run the goeBURST algorithm to st
 	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 	output = process.communicate()[0]
 
+
+def generatePublicLink(args, datasetID, currentRoot):
+
+	login(args, currentRoot)
+	print 'Generating Sharable Link...'
+	#make data set visible
+	bashCommand = 'curl --cookie jarfile -X PUT -d dataset_id='+ datasetID + ' -d change=true '+currentRoot+'/api/db/postgres/update/all/is_public'
+
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output = process.communicate()[0]
+
+	login(args, currentRoot)
+	#get sharable link
+	bashCommand = 'curl --cookie jarfile -X GET '+currentRoot+'/api/utils/publiclink?dataset_id='+ datasetID
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output = json.loads(process.communicate()[0])
+	
+	return output
+
+
 if __name__ == "__main__":
     main()
-
