e08b19796b28ee25cf26ab3902c4a3d9cfda64a5
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-10-26 11:33:08 +0100

    test

--- public/javascripts/App/indexApp/uploadfiles.js
+++ public/javascripts/App/indexApp/uploadfiles.js
@@ -160,7 +160,7 @@ function getLinks(data){
               clearInterval(checkI);
             }
           }) 
-        }, 3000);
+        }, 10000);
 
       }
       else if(data.dupProfiles.length > 0 || data.dupIDs.length >0){
