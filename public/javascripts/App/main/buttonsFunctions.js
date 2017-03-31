9c0147d320b3b81bdeb0d7a763d9e169b093c0cf
Bruno Goncalves <bgoncalves@Glaurung.lan>
2017-03-31 11:23:45 +0100

    test

--- public/javascripts/App/main/buttonsFunctions.js
+++ public/javascripts/App/main/buttonsFunctions.js
@@ -6,32 +6,53 @@ function loadButtonFunctions(){
 
 			$("#datasetInfobt").click(function(){
 
-				var toDialog = '<div id="divinfoDataset"></div>';
+				var toDialog = '<div id="divinfoDataset">';
+
+
 
 	        	$('#dialog').empty();
-				$('#dialog').append(toDialog);
 
 				var table = {};
-				table.headers = ['Data Set Name', 'Data Set Size', 'Type', 'Metadata', 'Max. Link Distance'];
-				if (graphObject.graphInput.data_type == 'profile' || graphObject.graphInput.data_type == 'fasta') table.headers.push('Profile Size');
-				table.data = [[graphObject.graphInput.dataset_name, graphObject.graphInput.nodes.length, graphObject.graphInput.data_type]];
-
-				if (graphObject.graphInput.metadata.length > 0) table.data[0].push('True');
-				else table.data[0].push('False');
-
-				table.data[0].push(graphObject.maxLinkValue);
-
-				if (graphObject.graphInput.data_type == 'profile' || graphObject.graphInput.data_type == 'fasta'){
-					table.data[0].push(graphObject.graphInput.nodes[0].profile.length);
+				//table.headers = ['Data Set Name', 'Data Set Size', 'Type', 'Metadata', 'Max. Link Distance'];
+				table.data = {'Data Set Name':graphObject.graphInput.dataset_name, 'Data Set Size':graphObject.graphInput.nodes.length, 'Data Type':graphObject.graphInput.data_type};
+				try{
+					table.data['Date'] = graphObject.graphInput.data_timestamp[0].split('T')[0];
+				}
+				catch(err){
+					table.data['Date'] = 'Undefined';
 				}
 
+				if (graphObject.graphInput.metadata.length > 0) table.data.Metadata = 'True';
+				else table.data.Metadata = 'False';
+
+				if (graphObject.graphInput.data_type == 'profile' || graphObject.graphInput.data_type == 'fasta') table.data['Original Profile Size'] = graphObject.graphInput.nodes[0].profile.length;
+				if(graphObject.graphInput.hasOwnProperty('goeburst_timer')) table.data['goeBURST Execution Time'] = graphObject.graphInput.goeburst_timer;
 				
+				table.data['Max. Link Distance'] = graphObject.maxLinkValue;
+
+				if(graphObject.graphInput.hasOwnProperty('missing_threshold')) table.data['Missing Data Threshold'] = graphObject.graphInput.missing_threshold;
+				if(graphObject.graphInput.hasOwnProperty('analysis_method')) table.data['Analysis Method'] = graphObject.graphInput.analysis_method;
+				table.data['goeBURST Profile Size'] = graphObject.graphInput.subsetProfiles[0].profile.length;
+				if(graphObject.graphInput.hasOwnProperty('parent_id')){
+					url = window.location.href.substring(0,window.location.href.lastIndexOf("/")) + '/' + graphObject.graphInput.parent_id;
+					table.data['Parent Data Set'] = '<a href="'+url+'">'+url+'</a>';
+				}
+				for(parameter in table.data){
+					toDialog += '<p><b>' + parameter + '</b> : ' + table.data[parameter];
+				}
+
+				toDialog += '</div>';
+
+				$('#dialog').append(toDialog);
+
+				/*
 				constructTable(graphObject.graphInput, table, 'infoDataset', function(){
 					$('#tableinfoDataset tfoot').remove();
 				});
+				*/
 
 				$('#dialog').dialog({
-			              height: $(window).height() * 0.20,
+			              height: $(window).height() * 0.40,
 			              width: $(window).width(),
 			              modal: true,
 			              resizable: true,
@@ -55,7 +76,7 @@ function loadButtonFunctions(){
 
 			var profileLength = graphObject.graphInput.nodes[0].profile.length;
 
-			$('#countProfileSize').append(' <b>' + profileLength + '</b>');
+			$('#countProfileSize').text(profileLength);
 
 		},
 
@@ -134,6 +155,10 @@ function loadButtonFunctions(){
 	            NodeSize(this.value, this.max, graphObject);
 	        });
 
+	        $('#SizeProperty').change(function(e){
+	            ChangeNodeSizeOption(graphObject, this.value);
+	        });
+
           	$('#NodeLabelSizeSlider').change(function(e){
             	LabelSize(this.value, graphObject, graphObject.nodeLabels, 'node');
           	});
@@ -176,6 +201,14 @@ function loadButtonFunctions(){
 
 	        $('#MassSlider').change(function(e){
 	            changeMass(this.value, this.max, graphObject);
+	        });
+
+	        $('#buttonChangeToOuter').click(function(e){
+	        	if(graphObject.withcenter){
+	        		changeColorToOuterRing(graphObject);
+	            	$('#outerColorLegendDiv').css({'display':'block'});
+	            	graphObject.withcenter = false;
+	        	}
 	        });
 
 	        $('#resetLayout').on('click', function(e){
@@ -328,49 +361,206 @@ function loadButtonFunctions(){
 			} 
 
 			$('#distanceButton').click(function(e){
-				if (graphObject.selectedNodes.length < 2) alert('To compute distances, first you need to select more than one node.');
-				else if (graphObject.selectedNodes.length >= 500) alert('To much nodes selected. The maximum number is currently 500.');
-	            else{
-	            	if(graphObject.graphInput.data_type == 'newick') getNewickDistances(graphObject);
-	            	else checkLociDifferences(graphObject);
-	            }
+				var cont = true;
+				if (graphObject.selectedNodes.length < 2){
+					var message = 'To compute distances, first you need to select more than one node.'; 
+					cont=false;
+				}
+				else if (graphObject.selectedNodes.length >= 500){
+					var message = 'To much nodes selected. The maximum number is currently 500.';
+					cont=false;
+				}
+
+				if(!cont){
+					var toDialog = '<div style="text-align: center;"><label>'+message+'</label></div>';
+
+			    	$('#dialog').empty();
+					$('#dialog').append(toDialog);
+					$('#dialog').dialog({
+				              height: $(window).height() * 0.15,
+				              width: $(window).width() * 0.2,
+				              modal: true,
+				              resizable: true,
+				              dialogClass: 'no-close success-dialog'
+				          });
+					return false;
+				}
+            	if(graphObject.graphInput.data_type == 'newick') getNewickDistances(graphObject);
+            	else checkLociDifferences(graphObject);
+
 	        });
 
 	        $('#savePositionsButton').click(function(e){
 	            saveTreePositions(graphObject);
 	        });
 
+	        if(graphObject.graphInput.data_type[0] != 'profile') $('#createSubset1').css({"display":"none"});
+
 	        $('#createSubset1').click(function(e){
 
+	        	graphObject.freezeSelection = true;
+
 	        	var toDialog = '<div style="text-align: center;"><label>Subset information:</label></div>' + 
-	        					'<h5>Dataset Name</h5>' +
+	        					'<label for="datasetNameSubset">Dataset Name</label>' +
 								'<input class="form-control input-sm" id="datasetNameSubset" type="text" placeholder="Select a name for the dataset" required/>'+
-								'<h5>Dataset Description</h5>' +
+								'<label for="dataset_description_Subset">Dataset Description</label>' +
 								'<input class="form-control input-sm" id="dataset_description_Subset" type="text" placeholder="Description"/>' + 
-								'<br>'+ 
-								'<div style="width:20%;float:center";><button id="okButtonsubset" class="btn btn-primary btn-md">OK</button></div>'+
-	        					'</div>';
+								'<br>'+
+								'<div id="profileAnalysisMethod">'+
+								'<label class="input-formats" for="sel_analysis_method">Analysis Method</label>'+	
+								'<select id="sel_analysis_method">'+
+									'<option value="core">Core Analysis</option>'+
+									'<option value="pres-abs">Presence/Absence</option>'+
+								'</select></div><br>'+
+								'<div id="missingcheckboxsubset" style="text-align:left;height:5%;">'+
+								'<label class="checkbox-inline"><input type="checkbox" id="missingchecksubset"/>Has missings</label>'+
+								'<label class="checkbox-inline"><input class="input-sm" id="missingdelimitersubset" type="text" placeholder="Missings character" required style="display:none"/></label>'+
+	        					'</div><br>'+
+	        					'<div class="col-md-12" id="div_threshold" style="display:none;">'+
+								'<div class="col-md-6">'+
+								'<span id="span_threshold">Threshold (Absent Loci)<input id="missingthreshold" type="range" value="100" min="0" max="100" required/></span>'+
+								'</div><div>'+
+								'<input type="text" style="width:30%;" id="textInput" value="100" disabled></div></div>'+
+	        					'<div style="width:100%;text-align:center"><button id="okButtonsubset" class="btn btn-primary btn-md">OK</button></div><br><div id="errorSubset" style="width:100%;text-align:center;"></div>';
 
 	        	$('#dialog').empty();
 				$('#dialog').append(toDialog);
 				$('#dialog').dialog({
-			              height: $(window).height() * 0.30,
-			              width: $(window).width() * 0.40,
+			              height: $(window).height() * 0.50,
+			              width: $(window).width() * 0.50,
 			              modal: true,
 			              resizable: true,
 			              dialogClass: 'no-close success-dialog'
 			          });
 
+				$('#sel_analysis_method').change(function(){
+					if($(this).val() == 'core') {
+						$('#missingcheckboxsubset').css({"display":"block"});
+						$('#missingchecksubset').css({"display":"block"});
+						$('#missingdelimitersubset').css({"display":"none"});
+						$('#div_threshold').css({"display":"none"});
+						document.getElementById('missingchecksubset').checked = false;
+					}
+					else{
+						$('#missingcheckboxsubset').css({"display":"block"});
+						$('#missingchecksubset').css({"display":"none"});
+						$('#missingdelimitersubset').css({"display":"block"});
+						$('#div_threshold').css({"display":"block"});
+						document.getElementById('missingchecksubset').checked = true;
+					}
+				});
+
+				$('#missingthreshold').change(function(){
+			  		document.getElementById('textInput').value=this.value; 
+				});
+
+
+				$('#missingchecksubset').click(function(){
+				  if ($(this).is(':checked')) $('#missingdelimitersubset').css({"display": "block"});
+				  else $('#missingdelimitersubset').css({"display": "none"});
+				  
+				});
+
+				$('#dialog').on('dialogclose', function(event) {
+				     graphObject.freezeSelection = false;
+				 });
 				$('#okButtonsubset').click(function(){
+
+					if(graphObject.selectedNodes.length == 0){
+						$('#errorSubset').empty();
+						$('#errorSubset').append('<label>First you need to select some nodes.</label>');
+						return false;
+					}
+					
 					var datasetN = $('#datasetNameSubset').val();
 					var descriptionS = $('#dataset_description_Subset').val();
-					toFiles = selectedDataToString(graphObject);
-	            	createSubset(toFiles, datasetN, descriptionS);
+					var missingsubset = true;
+					var missingCharsubset = '';
+
+					var analysis_method = 'core';
+					analysis_method = $('#sel_analysis_method').val();
+					var missing_threshold = '0';
+
+					if(document.getElementById('missingchecksubset').checked){
+					    missingsubset = true;
+					    missingCharsubset = $('#missingdelimitersubset').val();
+					    if (analysis_method == 'pres-abs') missing_threshold = $('#missingthreshold').val();
+					}
+
+					nodeNames = selectedDataNames(graphObject);
+					console.log(nodeNames);
+
+	            	createSubset(nodeNames, window.location.href.substr(window.location.href.lastIndexOf('/') + 1), datasetN, descriptionS, missingsubset, missingCharsubset, analysis_method, missing_threshold, function(data){
+	            		if(!data.error){
+	            			$('#dialog').dialog('close');
+	            			graphObject.freezeSelection = false;
+	            		}
+	            		else{
+	            			$('#createSubset1').trigger('click');
+	            			setTimeout(function(){
+	            				$('#dialog').append('<br><div style="width:100%;text-align:center;"><label>'+data.error+'</label></div>');
+	            			}, 200);
+	            		}
+	            	});
 				});
 	        });
 
+			$('#exportgoeBURST').click(function(){
+				exportgoeBURSTprofiles(graphObject);
+			});
+
+			$('#exportoriginal').click(function(){
+				exportprofiles(graphObject);
+			});
+
 	        $('#viewSequences').click(function(e){
 	            createMSA(graphObject);
+	        });
+
+	        var showExclusiveInfo = true;
+
+	        if(graphObject.graphInput.nodes[0].profile.length > 2000 && graphObject.graphInput.nodes.length > 3000){
+	        	$('#exclusiveLoci').css({'display': 'none'});
+	        	showExclusiveInfo = false;
+	        }
+
+	        if(graphObject.graphInput.nodes.length > 3000){
+	        	var toDialog = '<div style="text-align: center;"><label>Due to the large number of nodes, <b>NLV graph</b> option ';
+	        	if(!showExclusiveInfo) toDialog += 'and <b>Find Exclusive Loci</b> option are not available.';
+	        	else toDialog += 'is not available.';
+
+	        	toDialog += 'We are working to solve this problem.</label></div>';
+
+	        	$('#dialog').empty();
+				$('#dialog').append(toDialog);
+				$('#dialog').dialog({
+			              height: $(window).height() * 0.20,
+			              width: $(window).width() * 0.40,
+			              modal: true,
+			              resizable: true,
+			              dialogClass: 'no-close success-dialog'
+			          });
+	        }
+
+
+
+	        $('#exclusiveLoci').click(function(e){
+	            get_exclusive_loci(graphObject, function(){
+	            	if (graphObject.exclusive_loci.length == 0){
+	            		if (graphObject.selectedNodes.length != 0) var toDialog = '<div style="text-align: center;"><label>There are no unique profile positions related to the selected group.</label></div>';
+			        	else var toDialog = '<div style="text-align: center;"><label>Select one or more nodes to find exclusive loci.</label></div>';
+			        	$('#dialog').empty();
+						$('#dialog').append(toDialog);
+						$('#dialog').dialog({
+					              height: $(window).height() * 0.20,
+					              width: $(window).width() * 0.40,
+					              modal: true,
+					              resizable: true,
+					              dialogClass: 'no-close success-dialog'
+					          });
+	            	}
+	            	else write_exclusive_file(graphObject);
+	            });
 	        });
 
 	        $('#updateMetadata').click(function(e){
@@ -425,11 +615,24 @@ function loadButtonFunctions(){
 		        "max" : graphObject.maxLinkValue,
 		        "value" : graphObject.maxLinkValue 
 		    });
-	        
-		    $("#NLVnumber").attr({
-		        "max" : graphObject.graphInput.maxDistanceValue,
-		        "value" : 0
-		    });
+		    
+		    if(graphObject.graphInput.maxDistanceValue == -1){
+	        	$("#NLVnumber").attr({
+			        "value" : 0
+			    });
+	        }
+	        else {
+	        	$("#NLVnumber").attr({
+			        "max" : graphObject.graphInput.maxDistanceValue,
+			        "value" : 0
+			    });
+	        }
+
+		    if(!graphObject.graphInput.hasOwnProperty('distanceMatrix')){
+		    	$('#NLVgraph').css({"display": "none"});
+		    }
+
+
 
 	        $('#SplitTreeSlider').change(function(e){
 	            splitTree(graphObject, this.value);
@@ -459,6 +662,8 @@ function AddLinkLabels(graphObject){
   $('.link-label').css('display','block');
   if(graphObject.graphInput.data_type != "newick") {
   	$('#labelType').css({"display": "block"});
+  	if(graphObject.graphInput.missingsInfo[0]) $('#withmissings').css({"display": "block"});
+  	else $('#withmissings').css({"display": "none"});
   	$('#labelTypeNewick').css({"display": "none"});
   }
   else{
