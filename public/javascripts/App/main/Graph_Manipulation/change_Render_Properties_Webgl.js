29bc01ba8c03301d2c13c7708ff9ab692c7d7cc1
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-10 15:51:07 +0100

    add on complete queue worker handler

--- public/javascripts/App/main/Graph_Manipulation/change_Render_Properties_Webgl.js
+++ public/javascripts/App/main/Graph_Manipulation/change_Render_Properties_Webgl.js
@@ -428,7 +428,7 @@ function NLVgraph(graphObject, value) {
                         sourceKey = graph.sameNodeHas[sourceKey];
                         targetKey = graph.sameNodeHas[targetKey];
 
-                        //targetIndex = parseInt(countNodes) + parseInt(i);
+                        targetIndex = parseInt(countNodes) + parseInt(i);
                         console.log(countNodes, i, targetIndex, sourceKey, targetKey, graph.original_position_to_id);
 
                         if(targetKey.indexOf('TransitionNode') < 0){
