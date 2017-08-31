575120c6d818906b0b5f5ba7ab1bf7b359059a6d
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-31 14:34:10 +0100

    test collapse nodes

--- public/javascripts/App/main/Graph_Manipulation/change_Render_Properties_Webgl.js
+++ public/javascripts/App/main/Graph_Manipulation/change_Render_Properties_Webgl.js
@@ -506,7 +506,7 @@ function NLVcollapse(graphObject, value) {
             node_to_change = graphGL.getNode(graph.sameNodeHas[to_same_node_as[p][1]]);
             node_to_merge = graphGL.getNode(to_same_node_as[p][0]);
             node_to_change.data.isolates = node_to_change.data.isolates.concat(node_to_merge.data.isolates);
-            graph.mergedNodes[graph.sameNodeHas[to_same_node_as[p][1]]] = graph.mergedNodes[graph.sameNodeHas[to_same_node_as[p][1]]].concat(node_to_merge);
+            graph.mergedNodes[graph.sameNodeHas[to_same_node_as[p][1]]] = graph.mergedNodes[graph.sameNodeHas[to_same_node_as[p][1]]].concat(node_to_merge.data);
             console.log(node_to_change, node_to_merge);
         }
 
