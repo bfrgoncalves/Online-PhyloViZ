bcd591619bbf49e4aa8c730fbf43376172d7b970
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-08-31 13:45:24 +0100

    test collapse nodes

--- public/javascripts/App/main/Graph_Manipulation/change_Render_Properties_Webgl.js
+++ public/javascripts/App/main/Graph_Manipulation/change_Render_Properties_Webgl.js
@@ -513,9 +513,9 @@ function NLVcollapse(graphObject, value) {
             graphGL.removeLink(links_to_remove[l]);
         }
 
-        for(n in nodes_to_remove){
+        /*for(n in nodes_to_remove){
             graphGL.removeNode(graph.sameNodeHas[nodes_to_remove[n]]);
-        }
+        }*/
         console.log(to_same_node_as);
         console.log(links_to_add);
         console.log(links_to_remove);
