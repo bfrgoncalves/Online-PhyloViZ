8f41ec7502a432f19f2c5b5bd280f7359a9c636c
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-09-14 15:43:25 +0100

    fix distance matrix when merging nodes

--- public/javascripts/App/main/Graph_Manipulation/change_Render_Properties_Webgl.js
+++ public/javascripts/App/main/Graph_Manipulation/change_Render_Properties_Webgl.js
@@ -531,14 +531,12 @@ function NLVcollapse(graphObject, value) {
         already_merged[id_to_use] = true;
 
         graphGL.forEachLinkedNode(linkedNode.id, function(linkedNode2, link2){
-            if(link2.data.connectionStrength <= value){
-                if(nodes_to_remove.indexOf(linkedNode2.id) < 0 && id_to_use !== linkedNode2.id && link2.data.connectionStrength != value){
-                    LinkID = id_to_use + "👉 " + linkedNode2.id;
-                    links_to_add.push([id_to_use, linkedNode2.id, { connectionStrength: link2.data.connectionStrength , value: link2.data.connectionStrength, color: random_color}, LinkID]);
-                }
-                else if(nodes_to_remove.indexOf(linkedNode2.id) < 0 && id_to_use !== linkedNode2.id && link2.data.connectionStrength == value){
-                   deal_with_links(linkedNode2);
-                }
+            if(nodes_to_remove.indexOf(linkedNode2.id) < 0 && id_to_use !== linkedNode2.id && link2.data.connectionStrength > value){
+                LinkID = id_to_use + "👉 " + linkedNode2.id;
+                links_to_add.push([id_to_use, linkedNode2.id, { connectionStrength: link2.data.connectionStrength , value: link2.data.connectionStrength, color: random_color}, LinkID]);
+            }
+            else if(nodes_to_remove.indexOf(linkedNode2.id) < 0 && id_to_use !== linkedNode2.id && link2.data.connectionStrength == value){
+               deal_with_links(linkedNode2);
             }
         });
     }
