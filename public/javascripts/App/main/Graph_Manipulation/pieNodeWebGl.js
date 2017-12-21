297726d84eb0972515fbed329281f6c25c19a7ab
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-12-21 10:55:56 +0000

    anti-aliasing nodes

--- public/javascripts/App/main/Graph_Manipulation/pieNodeWebGl.js
+++ public/javascripts/App/main/Graph_Manipulation/pieNodeWebGl.js
@@ -161,6 +161,9 @@ function buildCircleNodeShader() {
                     */
 
                     'float r = 10, delta = 0.0, alpha = 1.0;',
+
+                    'vec2 cxy = 2.0 * gl_PointCoord - 1.0;',
+                    'r = dot(cxy, cxy);',
                     
                     'delta = fwidth(r);',
                     'alpha = 1.0 - smoothstep(1.0 - delta, 1.0 + delta, r);',
