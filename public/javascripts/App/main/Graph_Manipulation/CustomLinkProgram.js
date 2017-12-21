ac1452d2ff178998d162c2eb825a574f1f0c3c30
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-12-21 13:11:50 +0000

    anti-aliasing nodes

--- public/javascripts/App/main/Graph_Manipulation/CustomLinkProgram.js
+++ public/javascripts/App/main/Graph_Manipulation/CustomLinkProgram.js
@@ -26,7 +26,7 @@ function webglCustomLinkProgram(lineWidth) {
                 'float r = 0.1, delta = 0.0, alpha = 1.0;',
                 '#ifdef GL_OES_standard_derivatives',
                     'delta = fwidth(r);',
-                    'alpha = 0.5 - smoothstep(0.2 - delta, 0.2 + delta, r);',
+                    'alpha = 0.5 - smoothstep(0.2 - delta, 0.6 + delta, r);',
                 '#endif',
             '   gl_FragColor = color * alpha;',
             '}'
