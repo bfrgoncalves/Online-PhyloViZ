f5a7f575971fd8ed7be0749891af67a470bb13c7
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-12-22 15:25:38 +0000

    change root on config

--- routes/app/index.js
+++ routes/app/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var config = require('../../config.js');
 
 /* GET home page. */
-router.get(config.root_path + '/', function(req, res, next) {
+router.get('/', function(req, res, next) {
 
   res.render('indexApp', { 
   	  title: 'PHYLOViZ Online',
@@ -14,7 +14,7 @@ router.get(config.root_path + '/', function(req, res, next) {
 
 });
 /* GET input information. */
-router.get(config.root_path + '/inputinfo', function(req, res, next) {
+router.get('/inputinfo', function(req, res, next) {
   res.render('inputInformation', { 
   	  title: 'PHYLOViZ Online',
       isAuthenticated: req.isAuthenticated(), //function given by passport
@@ -24,7 +24,7 @@ router.get(config.root_path + '/inputinfo', function(req, res, next) {
 });
 
 /* GET input information. */
-router.get(config.root_path + '/tutorial', function(req, res, next) {
+router.get('/tutorial', function(req, res, next) {
   res.render('tutorial/tutorial', { 
       title: 'PHYLOViZ Online',
       isAuthenticated: req.isAuthenticated(), //function given by passport
