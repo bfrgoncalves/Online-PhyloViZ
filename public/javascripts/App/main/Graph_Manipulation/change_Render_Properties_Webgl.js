55a33148357acd248c6f2916ad6fe786bb3cd0ef
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-27 15:56:36 +0000

    test nlv

--- public/javascripts/App/main/Graph_Manipulation/change_Render_Properties_Webgl.js
+++ public/javascripts/App/main/Graph_Manipulation/change_Render_Properties_Webgl.js
@@ -449,6 +449,8 @@ function NLVgraph(graphObject, value) {
                             targetKey = graph.sameNodeHas[graph.nodes[targetIndex].key];
                         }
 
+                        console.log(graph.distanceMatrix[countNodes][i], countNodes, i, sourceKey, targetKey);
+
                         //console.log(sourceKey, targetKey, graph.sameNodeHas[graph.nodes[countNodes].key], graph.sameNodeHas[graph.nodes[targetIndex].key], countNodes, targetIndex);
 
                         //console.log(countNodes, i, targetIndex, sourceKey, targetKey, graph.original_position_to_id);
